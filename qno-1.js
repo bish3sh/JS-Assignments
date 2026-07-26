8a0e3fd3474a68a66bfae9b65bc79b40f331379e
Bishesh <bishesh.areteviva@gmail.com>
2026-07-26 23:08:09 +0545

    Qno 1

---
+++ qno-1.js
@@ -0,0 +1,13 @@
+let name='Bishesh';
+let age=23;
+const isStudent=true;
+
+console.log('my name is',name,' and i am ',age,'years old');
+
+const fruit=['apple','banana'];
+console.log(fruit[0]+fruit[1]);
+
+let x=5;
+let y= 2;
+const z=x*y;
+console.log(z);
