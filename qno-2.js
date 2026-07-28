cf37e62bfe8b2e6794b608cc6e3e2c784a24313c
Bishesh <bishesh.areteviva@gmail.com>
2026-07-28 08:46:12 +0545

    2nd qno

---
+++ qno-2.js
@@ -0,0 +1,19 @@
+function greetUser(name){
+    console.log('Hello '+name+'! Welcome to JS');
+}
+
+greetUser('Bishesh');
+
+function calcSquare(n){
+    return n*n;
+}
+
+let square=calcSquare(5);
+console.log(square);
+
+function addNum(a,b){
+    return a+b;
+}
+
+let sum=addNum(5,6);
+console.log(sum);
