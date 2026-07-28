6027b5e190b5dc7beafee16cc93703d37a7d02a3
Bishesh <bishesh.areteviva@gmail.com>
2026-07-28 09:19:28 +0545

    3rd qno

---
+++ qno-3.js
@@ -0,0 +1,19 @@
+// for (let i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+// let j=10;
+// while(j>=1){
+//     console.log(j);
+//     j--;
+// }
+
+// for (let i=1; i<=10; i++){
+//     console.log(i*5);
+// }
+
+j=2;
+while(j<=20){
+    console.log(j);
+    j+=2;
+}
